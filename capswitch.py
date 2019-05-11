ca5003bba88098282e29657d77f214704cc6a5a5
Luke Lopez <xlucas14@gmail.com>
2019-05-10 21:44:23 -0400

    Condensed pynput imports

--- capswitch.py
+++ capswitch.py
@@ -1,15 +1,14 @@
-from pynput import keyboard
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, Listener
 import pyperclip
 
 controller = Controller()
 
 def on_release(key):
-    if key == keyboard.Key.esc:
+    if key == Key.esc:
         # Stop listener
         return False
     
-    elif key == keyboard.Key.caps_lock:
+    elif key == Key.caps_lock:
         with controller.pressed(Key.ctrl_l):
             controller.press('c')
             controller.release('c')
@@ -22,6 +21,6 @@ def on_release(key):
             controller.release('v')
 
 # Collect events until released
-with keyboard.Listener(
+with Listener(
         on_release=on_release) as listener:
     listener.join()
